049dff2e85bccc6e01535706c7645d219798c979
GabrielRioo <gabrielpaivario@gmail.com>
2022-01-25 22:28:48 -0300

    Conceito de Componentes com NextJs

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,60 @@
+import appConfig from '../config.json';
+
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        border-sizing: border-box;
+      }
+
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+
+      html,body, #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+    `}</style>
+  )
+}
+
+function Title(props) {
+  const Tag =  props.tag;
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+    
+      <style jsx>{`
+        ${Tag} {
+          color: ${appConfig.theme.colors.neutrals['400']};
+        }
+        `}</style>
+    </>
+  )
+}
+
 function HomePage() {
-  return <div>Welcome to Next.Js!</div>
+  return (
+      <div>
+        <GlobalStyle />
+        <Title tag="h2">Boas vindas de volta!</Title>
+        <h2>Discord - Alura Matrix</h2>
+        
+        
+
+      </div>
+    ) 
 }
 
 export default HomePage;
